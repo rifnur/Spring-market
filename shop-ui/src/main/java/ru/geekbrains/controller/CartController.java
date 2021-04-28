870d2326b937abe9ef666b0b83f02ef6119ed92f
rifnur <rifnur69v@mail.ru>
2021-04-28 17:57:11 +0300

    Урок 6

--- shop-ui/src/main/java/ru/geekbrains/controller/CartController.java
+++ shop-ui/src/main/java/ru/geekbrains/controller/CartController.java
@@ -1,19 +1,26 @@
 package ru.geekbrains.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.controller.repr.CartItemRepr;
 import ru.geekbrains.controller.repr.ProductRepr;
 import ru.geekbrains.service.CartService;
 import ru.geekbrains.service.ProductService;
+import ru.geekbrains.service.model.LineItem;
+
+import java.util.Map;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 @RequestMapping("/cart")
 public class CartController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CartController.class);
 
     public final CartService cartService;
 
@@ -38,4 +45,19 @@ public class CartController {
         cartService.addProductQty(productRepr, "", "", cartItemRepr.getQty());
         return "redirect:/cart";
     }
+
+
+    @PostMapping(path = "/update_all_qty")
+    public String updateAllQty(@RequestParam Map<Long, Integer> paramMap) {
+        logger.info("Product Qty Map: {}", paramMap);
+        cartService.updateAllQty(paramMap);
+        return "redirect:/cart";
+    }
+
+    @DeleteMapping("{productId}/delete")
+    public String delete(@RequestParam("productId") Long productId) {
+        cartService.removeProduct(new ProductRepr(productId, null, null, null, null), "", "");
+        return "redirect:/cart";
+    }
+
 }
--- shop-ui/src/main/java/ru/geekbrains/service/CartServiceImpl.java
+++ shop-ui/src/main/java/ru/geekbrains/service/CartServiceImpl.java
@@ -1,21 +1,36 @@
 package ru.geekbrains.service;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import ru.geekbrains.controller.repr.ProductRepr;
 import ru.geekbrains.service.model.LineItem;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @Service
 @Scope(scopeName = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class CartServiceImpl implements CartService {
 
-    private final Map<LineItem, Integer> lineItems = new ConcurrentHashMap<>();
+    private final Map<LineItem, Integer> lineItems;
+
+    public CartServiceImpl() {
+        this.lineItems = new HashMap<>();
+    }
+
+    @JsonCreator
+    public CartServiceImpl(@JsonProperty("lineItems") List<LineItem> lineItems) {
+        this.lineItems = lineItems.stream().collect(Collectors.toMap(li -> li, LineItem::getQty));
+    }
 
     @Override
     public void addProductQty(ProductRepr productRepr, String color, String material, int qty) {
@@ -33,10 +48,32 @@ public class CartServiceImpl implements CartService {
             lineItems.remove(lineItem);
         }
     }
+    @Override
+    public void removeProduct(ProductRepr productRepr, String color, String material) {
+        LineItem lineItem = new LineItem(productRepr, color, material);
+        lineItems.remove(lineItem);
+    }
 
     @Override
     public List<LineItem> getLineItems() {
         lineItems.forEach(LineItem::setQty);
         return new ArrayList<>(lineItems.keySet());
     }
-}
+
+    @Override
+    public void updateAllQty(Map<Long, Integer> productIdQtyMap) {
+        // TODO
+        lineItems.forEach(LineItem::setQty);
+
+    }
+    @JsonIgnore
+    @Override
+    public BigDecimal getSubTotal() {
+        lineItems.forEach(LineItem::setQty);
+        return lineItems.keySet().stream()
+                .map(LineItem::getTotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+ }
+
+
